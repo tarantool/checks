f400d4023e07d35ba22209569e1ced05121aa9eb
Alexey Potapenko <a.potapenko@vkteam.ru>
2025-04-02 09:33:45 +0300

    ci: bump ubuntu version
    This patch bumps actions to use ubuntu-24.04 for fixing the following GitHub
    warning:
    
        The Ubuntu 20.04 Actions runner image will begin deprecation
        on 2025-02-01.
    
    Part of #TNTP-1918
    Closes #64

--- debian/prebuild.sh
+++ debian/prebuild.sh
@@ -2,4 +2,13 @@
 
 set -e -o pipefail
 
-curl -LsSf https://www.tarantool.io/release/1.10/installer.sh | sudo bash
+if [[ $DIST == "noble" ]] || [[ $DIST == "bookworm" ]]; then
+  curl -LsSf https://www.tarantool.io/release/3/installer.sh | sudo bash
+elif [[ $DIST == "impish" ]] || [[ $DIST == "jammy" ]]; then
+  curl -LsSf https://www.tarantool.io/release/2/installer.sh | sudo bash
+else
+  curl -LsSf https://www.tarantool.io/release/1.10/installer.sh | sudo bash
+fi
+
+sudo apt install -y tt
+tt version
